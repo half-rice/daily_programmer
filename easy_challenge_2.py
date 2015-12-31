7fa30bad445ef9a20530c2aba0d3e2b9616008ca
surrus <xsurrus@gmail.com>
2015-12-31 06:15:21 -0700

    added documentation

--- easy_challenge_2.py
+++ easy_challenge_2.py
@@ -1,5 +1,12 @@
-f = m = a = 0.0  # force, mass, accelleration
+# Your challenge for today is to create a calculator application that has use in 
+# your life. It might be an interest calculator, or it might be something that you 
+# can use in the classroom. For example, if you were in physics class, you might 
+# want to make a F = M * A calc.
 
+# EXTRA CREDIT: make the calculator have multiple functions! Not only should it 
+# be able to calculate F = M * A, but also A = F/M, and M = F/A!
+
+f = m = a = 0.0  # force, mass, accelleration
 
 def force(m,a):
   return m*a
